4dae378c07b3765643fb1376cc0479a249f4d90d
Vincent yu <yutiansut@qq.com>
2019-04-28 12:56:37 +0800

    Update install_opentradegateway.sh

--- install_opentradegateway.sh
+++ install_opentradegateway.sh
@@ -1,20 +1,12 @@
 ECHO 'install open-trade-gatway'
 apt update -y && apt install git gcc g++ libcurl4-openssl-dev automake autoconf libtool make -y 
 
-
-
 ECHO "install boost 1.70.0" 
-
-
-
 cd ~
-
 wget https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz && tar -zxvf boost_1_70_0.tar.gz
-
 cd boost_1_70_0 && ./bootstrap.sh && ./b2 && ./b2 install && ldconfig
 
-
-
+ECHO "install openssl/crypto" 
 cd ~
 wget https://www.openssl.org/source/openssl-1.0.2.tar.gz && tar zxvf openssl-1.0.2.tar.gz
 cd ~/openssl-1.0.2 && ./config && make && make install
@@ -29,5 +21,6 @@ ln -s /usr/local/ssl/lib/libcrypto.a /usr/local/lib/libcrypto.a
 echo "/usr/local/ssl/lib" >> /etc/ld.so.conf
 ldconfig -v
 
+#ECHO "CLONE OPEN-TRADE-GATEWAY and Install"
 #cd ~
 #git clone https://github.com/shinnytech/open-trade-gateway & cd open-trade-gateway & make & make install
