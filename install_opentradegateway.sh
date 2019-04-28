0965f88a6227acc548078cba0ee2c0b4f6b33dac
Vincent yu <yutiansut@qq.com>
2019-04-28 12:54:15 +0800

    Update install_opentradegateway.sh

--- install_opentradegateway.sh
+++ install_opentradegateway.sh
@@ -15,10 +15,19 @@ cd boost_1_70_0 && ./bootstrap.sh && ./b2 && ./b2 install && ldconfig
 
 
 
-# cd ~
-# wget https://www.openssl.org/source/openssl-1.0.2.tar.gz && tar zxvf openssl-1.0.2.tar.gz
-# cd ~/openssl-1.0.2 && ./config && make && make install
-
+cd ~
+wget https://www.openssl.org/source/openssl-1.0.2.tar.gz && tar zxvf openssl-1.0.2.tar.gz
+cd ~/openssl-1.0.2 && ./config && make && make install
+mv /usr/bin/openssl  /usr/bin/openssl.old
+mv /usr/include/openssl  /usr/include/openssl.old
+ln -s /usr/local/ssl/bin/openssl  /usr/bin/openssl
+ln -s /usr/local/ssl/include/openssl  /usr/include/openssl
+ln -s /usr/local/ssl/lib/libssl.so /usr/local/lib/libssl.so
+ln -s /usr/local/ssl/lib/libssl.a /usr/local/lib/libssl.a
+ln -s /usr/local/ssl/lib/libcrypto.so /usr/local/lib/libcrypto.so
+ln -s /usr/local/ssl/lib/libcrypto.a /usr/local/lib/libcrypto.a
+echo "/usr/local/ssl/lib" >> /etc/ld.so.conf
+ldconfig -v
 
 #cd ~
 #git clone https://github.com/shinnytech/open-trade-gateway & cd open-trade-gateway & make & make install
